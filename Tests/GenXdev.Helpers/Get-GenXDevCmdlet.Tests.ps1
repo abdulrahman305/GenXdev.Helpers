94828a779ed6cc3cb7de7f98be8a02d3bcc58dc5
René Vaessen <rene.vaessen@gmail.com>
2025-10-02 03:15:57 +0200

    Release 1.290.2025

--- Tests/GenXdev.Helpers/Get-GenXDevCmdlet.Tests.ps1
+++ Tests/GenXdev.Helpers/Get-GenXDevCmdlet.Tests.ps1
@@ -5,6 +5,6 @@ Pester\Describe 'Get-GenXDevCmdlet.Tests' {
         # run the script to get the cmdlets
 
         GenXdev.Helpers\Get-GenXDevCmdlet gcmds -ExactMatch | Microsoft.PowerShell.Core\ForEach-Object Name | Pester\Should -Contain 'Get-GenXDevCmdlet'
-        GenXdev.Helpers\Get-GenXDevCmdlet refactors -ExactMatch | Microsoft.PowerShell.Core\ForEach-Object Name | Pester\Should -Contain 'Get-Refactor'
+        GenXdev.Helpers\Get-GenXDevCmdlet cmds -ExactMatch | Microsoft.PowerShell.Core\ForEach-Object Name | Pester\Should -Contain 'Show-GenXDevCmdlet'
     }
 }
